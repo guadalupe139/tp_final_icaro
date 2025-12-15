ebc074fa747719d662693f16a6161f6928ee67c8
guadalupe atim <guadalupeatim13@gmail.com>
2025-12-15 18:25:37 -0300

    test runner

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,20 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "steps",
+        tags = "not @ignore",
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
